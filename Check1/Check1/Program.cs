5f0906f6e0f60e134a3069c2a1c6455dd656124a
Shubham Gaurav <shubhamgaurav019@gmail.com>
2020-06-30 14:20:41 +0530

    Check 1 updated

--- Check1/Check1/Program.cs
+++ Check1/Check1/Program.cs
@@ -5,8 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using LoggerPackage;
+using LoggerPack;
 
 namespace Check1
 {
@@ -19,10 +18,13 @@ namespace Check1
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            string[] Dsn = { "https://73b188f2b09743319fa80b493c831941@o409423.ingest.sentry.io/5281866" };
-            Class1.CreateHostBuilder(Dsn);
+ 
+           var app = Host.CreateDefaultBuilder(args);
+            string Dsn =  "https://73b188f2b09743319fa80b493c831941@o409423.ingest.sentry.io/5281866" ;
+            var obj = new Class1();
+            obj.UseGloabalLogger(app,Dsn);
 
-            return Host.CreateDefaultBuilder(args)
+            return app
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     
---
+++ Check1/LoggerPack/LoggerPack/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Sentry;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
+
+namespace LoggerPack
+{
+    public class Class1
+    {
+        public void UseGloabalLogger(IHostBuilder app, String dsn)
+        {
+            LogManager.Configuration = new LoggingConfiguration();
+
+            app.ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddDebug();
+                logging.AddNLog();
+            });
+
+
+         /*   LogManager.Configuration
+               .AddSentry(o =>
+               {
+
+                   o.Dsn = new Dsn(dsn);
+                    // Optionally specify a separate format for message
+                    o.Layout = "${message}";
+                    // Optionally specify a separate format for breadcrumbs
+                    o.BreadcrumbLayout = "${logger}: ${message}";
+
+                    // Debug and higher are stored as breadcrumbs (default is Info)
+                    o.MinimumBreadcrumbLevel = NLog.LogLevel.Debug;
+                    // Error and higher is sent as event (default is Error)
+                    o.MinimumEventLevel = NLog.LogLevel.Info;
+
+                    // Send the logger name as a tag
+                    o.AddTag("logger", "${logger}");
+
+                    // All Sentry Options are accessible here.
+                }); */
+        }
+    }
+}
